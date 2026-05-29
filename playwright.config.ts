a075f6bc43da475877bde5da68f814dd993dbbf3
Roy Schonberg <schonberg.r@gmail.com>
2026-05-29 23:21:03 +0300

    refactor(e2e): rename e2e/ to playwright/ for clarity

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from 'playwright/test'
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './playwright',
   use: {
     baseURL: 'http://localhost:5173/playGround-react/',
     ...devices['Desktop Chrome'],
